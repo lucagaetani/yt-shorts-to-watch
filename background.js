3a4788a4fe7d135360b35d95a65b1d5fbb10685d
Luca Gaetani <luca.gaetani76@gmail.com>
2024-02-29 19:47:21 +0100

    Minor changes.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url.includes("shorts")) {
+  if (changeInfo.status === "complete" && tab.url.includes("youtube.com/shorts")) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.update(tabs[0].id, { url: tabs[0].url.replace("shorts", "watch") });
     });
