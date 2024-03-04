c4e85a81b35ab6ddd2cee4afaa35da076f63bc10
Luca Gaetani <luca.gaetani76@gmail.com>
2024-03-04 23:49:06 +0100

    Fixed wrong behavior

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url.includes("youtube.com/shorts")) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.update(tabs[0].id, { url: tabs[0].url.replace("shorts", "watch") });
-    });
+  if (changeInfo.status === "complete") {
+    if (tab.url.includes("youtube.com/shorts")) {
+      chrome.tabs.update(tab.id, { url: tab.url.replace("shorts", "watch") });
+    }
   }
 });
