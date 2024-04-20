254f0471eb5bc61a5aed0331e1378f81102bf272
Luca Gaetani <luca.gaetani76@gmail.com>
2024-04-20 14:54:51 +0200

    Enable checkbox fully works. Version 1.2 pushed

--- background.js
+++ background.js
@@ -3,9 +3,14 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ isEnabled: true });
 });
 
+//If the extension is reactivated (but not uninstalled), activate it
+chrome.runtime.onStartup.addListener(() => {
+  chrome.storage.sync.set({ isEnabled: true });
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && tab.url && tab.url.includes("youtube.com/shorts")) {
-    chrome.storage.sync.get(['isEnabled'], function(data) {
+    chrome.storage.sync.get(['isEnabled'], (data) => {
       if (data.isEnabled) {
         const newUrl = tab.url.replace("shorts", "watch");
         chrome.tabs.update(tab.id, { url: newUrl });
--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const checkBox = document.querySelector('input[type="checkbox"]');
   const description = document.querySelector('.description');
-  const activatedText = "This extension is currently working. It will automatically change every YouTube Shorts in a normal YouTube video.";
-  const notActivatedText = "This extension is not currently working. Reactivate it to change every YouTube Shorts in a normal YouTube video.";
 
-  //Load the current state of the checkbox
-  chrome.storage.sync.get(['isEnabled'], function(data, event) {
+  // Load the current state of the checkbox
+  chrome.storage.sync.get(['isEnabled'], (data) => {
     checkBox.checked = data.isEnabled;
-    checkBox.checked ? description.textContent = activatedText : description.textContent = notActivatedText;
+    updateDescription(checkBox.checked);
   });
 
-  //Update the state when the checkbox is changed
-  checkBox.addEventListener('change', async function() {
+  // Update the state when the checkbox is changed
+  checkBox.addEventListener('change', () => {
     chrome.storage.sync.set({ isEnabled: checkBox.checked });
-    checkBox.checked ? description.textContent = activatedText : description.textContent = notActivatedText;
-    //If the extension is enabled and the user is watching a YouTube Shorts video, change the url
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    if (checkBox.checked && tab.url.includes("youtube.com/shorts")) {
-      const newUrl = tab.url.replace("shorts", "watch");
-      chrome.tabs.update(tab.id, { url: newUrl });
-    }
+    updateDescription(checkBox.checked);
+    let newIconsPath = checkBox.checked ? {
+      "16" : "../icons/icon16.png",
+      "32" : "../icons/icon32.png",
+      "48" : "../icons/icon48.png",
+      "128" : "../icons/icon128.png",
+    } : {
+      "16" : "../icons/icon16NA.png",
+      "32" : "../icons/icon32NA.png",
+      "48" : "../icons/icon48NA.png",
+      "128" : "../icons/icon128NA.png",
+    };
+    chrome.action.setIcon({ path: newIconsPath });
   });
+
+  const updateDescription = (isEnabled) => {
+    const activatedText = "This extension is currently working. It will automatically change every YouTube Shorts into a normal YouTube video.";
+    const notActivatedText = "This extension is not currently working. Reactivate it to change every YouTube Shorts into a normal YouTube video.";
+    description.textContent = isEnabled ? activatedText : notActivatedText;
+  }
 });
